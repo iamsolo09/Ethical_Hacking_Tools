964761b78bae7b3ec50b6184055b08cb92bb2869
iamsolo09 <61752573+iamsolo09@users.noreply.github.com>
2020-04-10 11:43:50 +0530

    Client_List if Null Output Changed
    Comments added and the output if client_list is null changed.

--- networkScanner.py
+++ networkScanner.py
@@ -3,6 +3,7 @@
 import scapy.all as scapy
 import optparse
 
+#Will get the arguments from the user 
 def get_arguments():
     parser = optparse.OptionParser()
     
@@ -15,30 +16,35 @@ def get_arguments():
 
     return options
 
+#Will scan the IP Address
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
+    arp_request = scapy.ARP(pdst=ip)#Packet for ARP Request
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")#Packet for broadcast
+    arp_request_broadcast = broadcast/arp_request#Make a new packet by combining the packets of Broadcast and ARP Request
 
+    #Timeout specified = 1s. This is to avoid waiting for a reponse a very long interval after scanning an IP Address
     answered = scapy.srp(arp_request_broadcast, verbose=False, timeout = 1)[0]
 
+    #Declaring a list to keep a dictionary of the client's information
     clients_list = []
     for element in answered:
-        client_dic = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dic)
+        #Each element of answered is a list of couple(packet sent, answer). Since I am only interested in the answer, I wrote element[1]
+        client_dic = {"ip": element[1].psrc, "mac": element[1].hwsrc}#psrc is the IP Address of the discovered client and hwsrc is the MAC Address of the discovered client
+        clients_list.append(client_dic)#Appending the dictionary client_dic to the list clients_list
 
         return clients_list
 
 
 def display_clients(clients_list):
+    #if clients_list is null, python wont be able to iterate through clients_list. Hence if it is null, no need to iterate. Rather display a message that no device is found.
     if clients_list is None:
-        print("No devices detected on network")
+        print("[-] No devices detected on network")
     else:
         for client in clients_list:
             print("IP Address\t\tMAC Address\n___________________________________________")
             print(client["ip"] + "\t\t" + client["mac"])
 
 
-options = get_arguments()
-scan_results = scan(options.ip)
-display_clients(scan_results)
+options = get_arguments()#get argument from the user
+scan_results = scan(options.ip)#scan the network for the provided IP Address or range of IP Address
+display_clients(scan_results)#Display the clients
